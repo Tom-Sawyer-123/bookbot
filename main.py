6497341b0546c7ffef85d4c6157e9b7fda07d37d
tsawyer <yungf4st@gmail.com>
2025-01-24 13:48:22 -0800

    added a book report to the main function

--- main.py
+++ main.py
@@ -9,15 +9,22 @@ def letters_in_book(book):
     for letter in letters:
             letter_count[letter] = letter_count.get(letter, 0) + 1
     return letter_count
-    
-    
-    return letter_count
+
+
+def book_report(words, letters):
+    print("--- Begin report of books/frankenstein.txt ---")
+    print(f"{words} words found in document")
+
 
 
 def main():
     with open("books/frankenstein.txt") as f:
         file_contents = f.read()
-        print(words_in_book(file_contents))
-        print(letters_in_book(file_contents))
+        print("--- Begin report of books/frankenstein.txt ---")
+        print(f"{words_in_book(file_contents)} words found in document")
+        print("")
+        for k,v in sorted(letters_in_book(file_contents).items(), key=lambda p:p[1], reverse=True):
+            if k.isalpha():
+                print(f"The '{k}' character was printed {v} times")
 
 main()
